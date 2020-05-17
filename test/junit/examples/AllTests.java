6ecb5274809b5676b0f0592509af4c78c3ac0778
kiyota takeshi <kiyota@tagbangers.co.jp>
2020-05-17 23:41:58 +0900

    test suite class

---
+++ test/junit/examples/AllTests.java
@@ -0,0 +1,14 @@
+package junit.examples;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * テストスイートクラス
+ * 複数のテストクラスを束ねてテストを実行する
+ */
+//@RunWith(Suite.class)
+//@Suite.SuiteClasses({ FooTest.class, BarTest.class })
+//public class AllTests {
+//
+//}
